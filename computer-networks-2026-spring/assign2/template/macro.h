dd6a29ce7422a318a9b1c2185ca0509df55ca497
askrid <joonwoo3023@gmail.com>
2026-04-14 16:22:02 +0900

    unzip

---
+++ computer-networks-2026-spring/assign2/template/macro.h
@@ -0,0 +1,17 @@
+#ifndef MACRO_H_
+#define MACRO_H_
+
+#define TRUE  1
+#define FALSE 0
+
+#define MAX_HDR  (1024)          /* maximum header length */
+
+/* debug trace */
+#ifndef OFFTRACE
+#define TRACE(fmt, msg...) \
+    fprintf(stderr, "%s (%s:%d) " fmt, __FUNCTION__, __FILE__, __LINE__, ##msg)
+#else    
+    #define TRACE(fmt, msg...) (void)0
+#endif
+
+#endif
